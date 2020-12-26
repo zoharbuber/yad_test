84613c7192be31e698b24a65a338397ef972eda6
zoharbuber <76256891+zoharbuber@users.noreply.github.com>
2020-12-26 17:20:41 +0200

    Update sql_advertisement.py

--- sql_advertisement.py
+++ sql_advertisement.py
@@ -3,13 +3,34 @@ from yad2_query import create_table, select_query, insert_query
 from advertisement import Advertisement
 
 
-conn = sqlite3.connect('yad2_db')
-cursor = conn.cursor()
+class sql_queries:
+    def __init__(self):
+        self.adv_data = Advertisement()
 
-cursor.execute(create_table)
+    def run_query(self):
+        conn = sqlite3.connect('yad2_db')
+        cursor = conn.cursor()
 
-conn.commit()
+        try:
+            cursor.execute(create_table)
+            conn.commit()
+        except Exception as e:
+            print('error is: %s' % e)
 
-cursor.execute(insert_query)
+        try:
+            cursor.execute(select_query)
+            conn.commit()
+        except Exception as e:
+            print('error is: %s' % e)
 
-conn.commit()
+        try:
+            for price in self.adv_data.prices:
+                cursor.execute(insert_query, price)
+                conn.commit()
+        except Exception as e:
+            print('error is: %s' % e)
+
+
+if __name__ == '__main__':
+    sql = sql_queries()
+    sql.run_query()
