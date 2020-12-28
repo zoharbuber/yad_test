516faf43f94d73cc8ac19145c11d9d88642034ff
zoharbuber <76256891+zoharbuber@users.noreply.github.com>
2020-12-28 08:55:15 +0200

    Update sql_advertisement.py

--- sql_advertisement.py
+++ sql_advertisement.py
@@ -1,36 +1,25 @@
 import sqlite3
 from yad2_query import create_table, select_query, insert_query
-from advertisement import Advertisement
+from Advertisement import Advertisement
 
 
-class sql_queries:
+class AdvertisementDAO:
     def __init__(self):
-        self.adv_data = Advertisement()
+        self.conn = sqlite3.connect('yad2_db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Advertisement (
+            streets text,
+            prices text,
+            apartment_types text,
+            cities text,
+            areas text,
+            secondary_areas text)
+        ''')
+        self.conn.commit()
 
-    def run_query(self):
-        conn = sqlite3.connect('yad2_db')
-        cursor = conn.cursor()
+    def insert_advertisements(self, advertisements):
+        for advertisement in advertisements:
+            self.cursor.execute(insert_query, [advertisement.price])
+            self.conn.commit()
 
-        try:
-            cursor.execute(create_table)
-            conn.commit()
-        except Exception as e:
-            print('error is: %s' % e)
-
-        try:
-            cursor.execute(select_query)
-            conn.commit()
-        except Exception as e:
-            print('error is: %s' % e)
-
-        try:
-            for price in self.adv_data.prices:
-                cursor.execute(insert_query, price)
-                conn.commit()
-        except Exception as e:
-            print('error is: %s' % e)
-
-
-if __name__ == '__main__':
-    sql = sql_queries()
-    sql.run_query()
