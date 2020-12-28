b839fd0a069089db362a454a4656a7546feb8c85
zoharbuber <76256891+zoharbuber@users.noreply.github.com>
2020-12-28 08:57:54 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from Advertisement import Advertisement
+from scraper import Scraper
+from advertisement_dao import AdvertisementDAO
+
+if __name__ == '__main__':
+    scraper = Scraper()
+    advertisements = scraper.scrape_yad2()
+    advertisement_dao = AdvertisementDAO()
+    advertisement_dao.insert_advertisements(advertisements)
