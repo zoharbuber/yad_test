87d9bb6cd5664425d92c6f04480d3579f77aa153
zoharbuber <76256891+zoharbuber@users.noreply.github.com>
2020-12-28 08:54:03 +0200

    Update Advertisement.py

--- Advertisement.py
+++ Advertisement.py
@@ -1,38 +1,8 @@
-from bs4 import BeautifulSoup as bs
-import requests
-
-
 class Advertisement:
-    def __init__(self):
-        self.streets = []
-        self.prices = []
-        self.apartment_types = []
-        self.cities = []
-        self.areas = []
-        self.secondary_areas = []
-        self.scrape_yad2()
-
-    def scrape_yad2(self):
-        url = 'https://www.yad2.co.il/realestate/rent'
-        try:
-            r = requests.get(url, headers={'user-agent': 'Mozilla/7.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'})
-            soup = bs(r.content)
-            all_advertisements = soup.find_all("div", {'class': 'feeditem table'})
-            for data in all_advertisements:
-                self.prices.append(str(data.find("div", {'class': 'price'}).contents[0]).replace("\n", "").replace(" ", ""))
-                self.streets.append(str(data.find("span", {'class': 'title'}).contents[0]).replace("\n", "").replace(" ", ""))
-                apartment_info = str(data.find("span", {'class': 'subtitle'}).contents[0]).split(",")
-                if len(apartment_info) == 4:
-                    self.apartment_types.append(str(apartment_info[0]))
-                    self.areas.append(str(apartment_info[1]))
-                    self.secondary_areas.append(str(apartment_info[2]))
-                    self.cities.append(str(apartment_info[3]))
-                    continue
-
-                self.apartment_types.append(str(apartment_info[0]))
-                self.areas.append(str(apartment_info[1]))
-
-                if len(apartment_info) > 2:
-                    self.cities.append(str(apartment_info[2]))
-        except Exception as e:
-            print('error is: %s' % e)
+    def __init__(self, street, price, apartment_type, city, area, secondary_area):
+        self.street = street
+        self.price = price
+        self.apartment_types = apartment_type
+        self.city = city
+        self.area = area
+        self.secondary_area = secondary_area
